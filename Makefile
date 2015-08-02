2a94a6129eec78699d7a467f06775d495dcc58a0
David Macek <david.macek.0@gmail.com>
2015-08-02 16:55:17 +0200

    Switched to pure C; removed pathtools dependency; moved more stuff to wide strings; added strip to the build process

--- Makefile
+++ Makefile
@@ -1,19 +1,19 @@
 all: msys2.exe mingw32.exe mingw64.exe
 
-pathtools.o: pathtools.c pathtools.h
-	g++ -Wall -Wextra -o $@ -c $^
-
 msys2.res: launcher.rc msys2.ico
 	windres -O COFF -o $@ $< -DMSYSTEM=MSYS -DICONFILE=msys2.ico
-msys2.exe: launcher.cpp pathtools.o msys2.res
-	g++ -Wall -Wextra -static-libstdc++ -static -mwindows -o $@ $^ -luuid -lshlwapi -lpsapi -DMSYSTEM=MSYS -DEXENAME=$@
+msys2.exe: launcher.c msys2.res
+	gcc -Wall -Wextra -static -mwindows -o $@ $^ -luuid -lshlwapi -lpsapi -DMSYSTEM=MSYS
+	strip $@
 
 mingw32.res: launcher.rc mingw32.ico
 	windres -O COFF -o $@ $< -DMSYSTEM=MINGW32 -DICONFILE=mingw32.ico
-mingw32.exe: launcher.cpp pathtools.o mingw32.res
-	g++ -Wall -Wextra -static-libstdc++ -static -mwindows -o $@ $^ -luuid -lshlwapi -lpsapi -DMSYSTEM=MINGW32 -DEXENAME=$@
+mingw32.exe: launcher.c mingw32.res
+	gcc -Wall -Wextra -static -mwindows -o $@ $^ -luuid -lshlwapi -lpsapi -DMSYSTEM=MINGW32
+	strip $@
 
 mingw64.res: launcher.rc mingw64.ico
 	windres -O COFF -o $@ $< -DMSYSTEM=MINGW64 -DICONFILE=mingw64.ico
-mingw64.exe: launcher.cpp pathtools.o mingw64.res
-	g++ -Wall -Wextra -static-libstdc++ -static -mwindows -o $@ $^ -luuid -lshlwapi -lpsapi -DMSYSTEM=MINGW64 -DEXENAME=$@
+mingw64.exe: launcher.c mingw64.res
+	gcc -Wall -Wextra -static -mwindows -o $@ $^ -luuid -lshlwapi -lpsapi -DMSYSTEM=MINGW64
+	strip $@
--- launcher.c
+++ launcher.c
@@ -11,11 +11,11 @@
 #include <processthreadsapi.h>
 #include <psapi.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <stdio.h>
-#include <string>
+#include <wchar.h>
 
 #include "macros.h"
-#include "pathtools.h"
 
 static void ShowError(const char* desc, const char* err, const long code) {
 	char msg[1024];
@@ -35,8 +35,8 @@ static void ShowLastError(const char* desc) {
 }
 
 
-PROCESS_INFORMATION StartChild(const char* cmdline) {
-	STARTUPINFO si;
+PROCESS_INFORMATION StartChild(const wchar_t* cmdline) {
+	STARTUPINFOW si;
 	PROCESS_INFORMATION pi;
 	DWORD code;
 
@@ -44,7 +44,7 @@ PROCESS_INFORMATION StartChild(const char* cmdline) {
 	si.cb = sizeof(si);
 	ZeroMemory(&pi, sizeof(pi));
 	SetLastError(0);
-	code = CreateProcess(NULL, (char*)cmdline, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
+	code = CreateProcessW(NULL, (wchar_t*)cmdline, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
 	if (code == 0) {
 		ShowLastError("Could not start the shell");
 		return pi;
@@ -62,7 +62,6 @@ static int SetEnv(const char* msystem) {
 	env = (char*)alloca(strlen(prefix) + strlen(msystem) + 1);
 	strcpy(env, prefix);
 	strcat(env, msystem);
-
 	code = putenv(env);
 	if (code != 0) {
 		err = strerror(errno);
@@ -74,50 +73,51 @@ static int SetEnv(const char* msystem) {
 }
 
 struct handle_data {
-    unsigned long process_id;
-    HWND best_handle;
+	unsigned long process_id;
+	HWND best_handle;
 };
 
 
 BOOL is_main_window(HWND handle)
 {
-    return GetWindow(handle, GW_OWNER) == (HWND)0 && IsWindowVisible(handle);
+	return GetWindow(handle, GW_OWNER) == (HWND)0 && IsWindowVisible(handle);
 }
 
 
 BOOL CALLBACK enum_windows_callback(HWND handle, LPARAM lParam)
 {
-    struct handle_data* data = (struct handle_data*)lParam;
-    unsigned long process_id = 0;
-    GetWindowThreadProcessId(handle, &process_id);
-    if (data->process_id != process_id || !is_main_window(handle)) {
-        return TRUE;
-    }
-    data->best_handle = handle;
-    return FALSE;
+	struct handle_data* data = (struct handle_data*)lParam;
+	unsigned long process_id = 0;
+	GetWindowThreadProcessId(handle, &process_id);
+	if (data->process_id != process_id || !is_main_window(handle)) {
+		return TRUE;
+	}
+	data->best_handle = handle;
+	return FALSE;
 }
 
 HWND find_main_window(unsigned long process_id)
 {
-    struct handle_data data;
-    data.process_id = process_id;
-    data.best_handle = 0;
-    EnumWindows(enum_windows_callback, (LPARAM)&data);
-    return data.best_handle;
+	struct handle_data data;
+	data.process_id = process_id;
+	data.best_handle = 0;
+	EnumWindows(enum_windows_callback, (LPARAM)&data);
+	return data.best_handle;
 }
 
 
 
-HRESULT SetAppUserModelProperty(IPropertyStore* store, const PROPERTYKEY key, LPCWSTR value) {
+HRESULT SetAppUserModelProperty(IPropertyStore* store, const PROPERTYKEY* key, LPCWSTR value) {
 	PROPVARIANT propVariant;
 	HRESULT hr;
 
-	hr = InitPropVariantFromString(value, &propVariant);
+	propVariant.vt = VT_LPWSTR;
+	hr = SHStrDupW(value, &propVariant.pwszVal);
 	if (FAILED(hr)) {
 		return hr;
 	}
 
-	hr = store->SetValue(key, propVariant);
+	hr = store->lpVtbl->SetValue(store, key, &propVariant);
 	if (FAILED(hr)) {
 		return hr;
 	}
@@ -129,33 +129,44 @@ HRESULT SetAppUserModelProperty(IPropertyStore* store, const PROPERTYKEY key, LP
 int main(int argc, char* argv[]) {
 	PROCESS_INFORMATION child;
 	DWORD code;
+	int res;
 	HWND window;
 	IPropertyStore* store;
 	HRESULT hr;
-	char* cmdline;
-	const char cmd[] = "/usr/bin/mintty.exe -i /msys2.ico /usr/bin/bash --login -i";
-	char msysdirA[PATH_MAX];
-	wchar_t msysdirW[PATH_MAX];
+	size_t buflen;
+	wchar_t* buf;
+	wchar_t msysdir[PATH_MAX], exepath[PATH_MAX];
 
 	UNUSED(argc);
 	UNUSED(argv);
 
-	get_executable_path(argv[0], msysdirA, sizeof(msysdirA) / sizeof(msysdirA[0]));
-	if (strrchr(msysdirA, '/') != NULL) {
-		strrchr(msysdirA, '/')[1] = '\0';
+	code = GetModuleFileNameW(NULL, exepath, sizeof(exepath) / sizeof(exepath[0]));
+	if (code == 0) {
+		ShowLastError("Could not determine executable path");
+		return __LINE__;
 	}
-	mbstowcs(msysdirW, msysdirA, sizeof(msysdirW) / sizeof(msysdirW[0]));
 
-	cmdline = (char*)alloca(strlen(msysdirA) + strlen(cmd) + 1);
-	strcpy(cmdline, msysdirA);
-	strcat(cmdline, cmd);
+	while (wcschr(exepath, L'\\') != NULL) {
+		wcschr(exepath, L'\\')[0] = L'/';
+	}
+	wcscpy(msysdir, exepath);
+	if (wcsrchr(msysdir, L'/') != NULL) {
+		wcsrchr(msysdir, L'/')[1] = L'\0';
+	}
+	buflen = wcslen(msysdir) + 1000;
+	buf = (wchar_t*)alloca(buflen * sizeof(wchar_t));
 
 	code = SetEnv(STRINGIFY_A(MSYSTEM));
 	if (code == 0) {
 		return __LINE__;
 	}
 
-	child = StartChild(cmdline);
+	res = swprintf(buf, buflen, L"%s/usr/bin/mintty.exe -i /msys2.ico /usr/bin/bash --login -i", msysdir);
+	if (res < 0) {
+		return __LINE__;
+	}
+
+	child = StartChild(buf);
 	if (child.hProcess == NULL) {
 		return __LINE__;
 	}
@@ -167,37 +178,37 @@ int main(int argc, char* argv[]) {
 			continue;
 		}
 
-		hr = SHGetPropertyStoreForWindow(window, IID_PPV_ARGS(&store));
+		hr = SHGetPropertyStoreForWindow(window, &IID_IPropertyStore, (void**)&store);
 		if (FAILED(hr)) {
 			return __LINE__;
 		}
 
-		hr = SetAppUserModelProperty(store, PKEY_AppUserModel_ID, L"MSYS2.Shell." STRINGIFY_W(MSYSTEM) L".0");
+		hr = SetAppUserModelProperty(store, &PKEY_AppUserModel_ID, L"MSYS2.Shell." STRINGIFY_W(MSYSTEM) L".2");
 		if (FAILED(hr)) {
 			return __LINE__;
 		}
 
-		hr = SetAppUserModelProperty(store, PKEY_AppUserModel_RelaunchCommand, (std::wstring(msysdirW) + (L"/" STRINGIFY_W(EXENAME))).c_str());
+		hr = SetAppUserModelProperty(store, &PKEY_AppUserModel_RelaunchDisplayNameResource, L"MSYS2/" STRINGIFY_W(MSYSTEM) L" shell");
 		if (FAILED(hr)) {
 			return __LINE__;
 		}
 
-		hr = SetAppUserModelProperty(store, PKEY_AppUserModel_RelaunchDisplayNameResource, L"MSYS2 " STRINGIFY_W(MSYSTEM) L" shell");
+		hr = SetAppUserModelProperty(store, &PKEY_AppUserModel_RelaunchCommand, exepath);
 		if (FAILED(hr)) {
 			return __LINE__;
 		}
 
-		hr = SetAppUserModelProperty(store, PKEY_AppUserModel_RelaunchIconResource, (std::wstring(msysdirW) + L"/msys2.ico").c_str());
+		hr = SetAppUserModelProperty(store, &PKEY_AppUserModel_RelaunchIconResource, exepath);
 		if (FAILED(hr)) {
 			return __LINE__;
 		}
 
-		hr = store->Commit();
+		hr = store->lpVtbl->Commit(store);
 		if (FAILED(hr)) {
 			return __LINE__;
 		}
 
-		store->Release();
+		store->lpVtbl->Release(store);
 		store = NULL;
 		break;
 	}
