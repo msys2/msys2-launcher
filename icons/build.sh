c2aaca5cd85cfb93d2415ed05fed769272026743
Christoph Reiter <reiter.christoph@gmail.com>
2021-03-23 18:53:32 +0100

    Rebuild the icons based on SVG sources
    The SVG was created by tracing with Inkscape

---
+++ icons/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+cp icon-large.svg temp-large.svg
+cp icon-medium.svg temp-medium.svg
+cp icon-small.svg temp-small.svg
+sed -i 's/#00ff00/#ffffff/g' temp-large.svg temp-medium.svg temp-small.svg
+sed -i "s/#0000ff/$1/g" temp-large.svg temp-medium.svg temp-small.svg
+sed -i 's/#ff0000/#ef7e19/g' temp-large.svg temp-medium.svg temp-small.svg
+rsvg-convert temp-small.svg -w 16 -h 16 --output temp-16.png
+rsvg-convert temp-small.svg -w 32 -h 32 --output temp-32.png
+rsvg-convert temp-medium.svg -w 48 -h 48 --output temp-48.png
+rsvg-convert temp-large.svg -w 64 -h 64 --output temp-64.png
+rsvg-convert temp-large.svg -w 128 -h 128 --output temp-128.png
+convert temp-16.png temp-32.png temp-48.png temp-64.png temp-128.png +dither -colors 255 "$2"
+rm temp-*.png temp-*.svg
