f810bdba12938a7e1733eb724f529847a3823496
Christoph Reiter <reiter.christoph@gmail.com>
2021-03-24 18:56:02 +0100

    Add higher res variants to the icons
    Windows wants a 256x256 for the "large" display setting, otherwise it shows
    a lower res and adds a border.

--- icons/build.sh
+++ icons/build.sh
@@ -12,6 +12,7 @@ rsvg-convert temp-small.svg -w 16 -h 16 --output temp-16.png
 rsvg-convert temp-small.svg -w 32 -h 32 --output temp-32.png
 rsvg-convert temp-medium.svg -w 48 -h 48 --output temp-48.png
 rsvg-convert temp-large.svg -w 64 -h 64 --output temp-64.png
-rsvg-convert temp-large.svg -w 128 -h 128 --output temp-128.png
-convert temp-16.png temp-32.png temp-48.png temp-64.png temp-128.png +dither -colors 255 "$2"
+rsvg-convert temp-large.svg -w 96  -h 96 --output temp-96.png
+rsvg-convert temp-large.svg -w 256 -h 256 --output temp-256.png
+convert temp-16.png temp-32.png temp-48.png temp-64.png temp-96.png temp-256.png +dither -colors 255 "$2"
 rm temp-*.png temp-*.svg
